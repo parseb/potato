9bb9099831b2064078f7ff890e8e13d22b7b3841
parseb <petra306@protonmail.com>
2022-07-09 17:35:45 +0300

    potato on rails init

---
+++ db/schema.rb
@@ -0,0 +1,44 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[7.0].define(version: 2022_07_09_142855) do
+  create_table "bananas", force: :cascade do |t|
+    t.string "name"
+    t.string "type"
+    t.string "age"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "baskets", force: :cascade do |t|
+    t.string "data_url"
+    t.string "farmer_address"
+    t.string "customer_address"
+    t.string "state"
+    t.string "nft_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "consumers", force: :cascade do |t|
+    t.string "address"
+    t.string "name"
+    t.string "score"
+    t.string "pending"
+    t.string "claimed"
+    t.string "data_url"
+    t.string "area_address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end
